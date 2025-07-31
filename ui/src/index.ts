de003b1ed4e2ec6510ed97fe09d045d73ffc85d4
denchouka <denchouka@163.com>
2025-07-31 21:38:25 +0800

    修改插件配置

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,29 +1,43 @@
 import { definePlugin } from '@halo-dev/console-shared'
-import HomeView from './views/HomeView.vue'
-import { IconPlug } from '@halo-dev/components'
-import { markRaw } from 'vue'
+import LinksHealthMonitor from './views/LinksHealthMonitor.vue'
+import { VLoading, IconDashboard } from '@halo-dev/components'
+import { markRaw, defineAsyncComponent } from 'vue'
 
 export default definePlugin({
   components: {},
   routes: [
     {
-      parentName: 'Root',
+      parentName: 'ToolsRoot',
       route: {
-        path: '/example',
-        name: 'Example',
-        component: HomeView,
+        path: '/linksHealth',
+        name: 'LinksHealthMonitor',
+        component: LinksHealthMonitor,
         meta: {
-          title: '示例页面',
+          title: 'Links Health Monitor',
           searchable: true,
           menu: {
-            name: '示例页面',
-            group: '示例分组',
-            icon: markRaw(IconPlug),
+            name: 'Links Health Monitor',
+            group: "tool",
+            icon: markRaw(IconDashboard),
             priority: 0,
+            mobile: true
           },
         },
       },
     },
   ],
-  extensionPoints: {},
+  extensionPoints: {"plugin:self:tabs:create": () => {
+      return [
+        {
+          id: "links-health-monitor-settings",
+          label: "设置",
+          component: defineAsyncComponent({
+            loader: () => import("./views/LinksHealthSettings.vue"),
+            loadingComponent: VLoading,
+          }),
+          permissions: ["*"],
+        },
+      ];
+    },
+  },
 })
