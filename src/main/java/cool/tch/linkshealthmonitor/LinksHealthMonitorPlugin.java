2c2af687d6bc5684e12a2334978a3f6f496868e9
denchouka <denchouka@163.com>
2025-08-22 22:10:35 +0800

    注册自定义模型

--- src/main/java/cool/tch/linkshealthmonitor/LinksHealthMonitorPlugin.java
+++ src/main/java/cool/tch/linkshealthmonitor/LinksHealthMonitorPlugin.java
@@ -1,6 +1,11 @@
 package cool.tch.linkshealthmonitor;
 
+import cool.tch.linkshealthmonitor.task.LinksHealthMonitorResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
+import run.halo.app.extension.Scheme;
+import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 import run.halo.app.plugin.PluginContext;
 
@@ -13,19 +18,29 @@ import run.halo.app.plugin.PluginContext;
  * @since 1.0.0
  */
 @Component
+@EnableAsync
+@EnableScheduling
 public class LinksHealthMonitorPlugin extends BasePlugin {
 
-    public LinksHealthMonitorPlugin(PluginContext pluginContext) {
+    private final SchemeManager schemeManager;
+
+    public LinksHealthMonitorPlugin(PluginContext pluginContext, SchemeManager schemeManager) {
         super(pluginContext);
+        this.schemeManager = schemeManager;
     }
 
     @Override
     public void start() {
         System.out.println("插件启动成功！");
+        // 注册自定义模型
+        schemeManager.register(LinksHealthMonitorResult.class);
     }
 
     @Override
     public void stop() {
         System.out.println("插件停止！");
+        // 取消注册自定义模型
+        Scheme scheme = schemeManager.get(LinksHealthMonitorResult.class);
+        schemeManager.unregister(scheme);
     }
 }
