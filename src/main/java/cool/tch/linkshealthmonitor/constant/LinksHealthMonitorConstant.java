c25858b8b1464e90252d576bce2117486dfdb234
denchouka <denchouka@163.com>
2025-08-22 22:06:20 +0800

    新增自定义常量

---
+++ src/main/java/cool/tch/linkshealthmonitor/constant/LinksHealthMonitorConstant.java
@@ -0,0 +1,19 @@
+package cool.tch.linkshealthmonitor.constant;
+
+/**
+ * @Author Denchouka
+ * @Date 2025/8/10 22:29
+ * @Desc 常量
+ */
+public class LinksHealthMonitorConstant {
+
+    public static final String LINKS_HEALTH_MONITOR = "Links Health Monitor";
+
+    public static final String LINKS_HEALTH_MONITOR_DESC = "友情链接健康监测插件";
+
+    // 默认的cron表达式：每天0点执行一次
+    public static final String DEFAULT_CRON = "0 0 0 * * ?";
+
+    // 自定义模型元数据名称前缀
+    public static final String CUSTOM_MODEL_METADATA_NAME_PREFIX = "links-health-";
+}
