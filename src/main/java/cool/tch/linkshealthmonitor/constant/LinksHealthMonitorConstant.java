c1d33822cf8ef20731d50ee03481fe9bfd0ad827
denchouka <denchouka@163.com>
2025-08-31 17:55:54 +0800

    新增常量

--- src/main/java/cool/tch/linkshealthmonitor/constant/LinksHealthMonitorConstant.java
+++ src/main/java/cool/tch/linkshealthmonitor/constant/LinksHealthMonitorConstant.java
@@ -23,4 +23,16 @@ public class LinksHealthMonitorConstant {
 
     // 服务关闭时,等待所有正在执行的任务完成，最多等待的秒数
     public static final int CUSTOM_THREAD_AWAIT_TIME = 60;
+
+    // 默认的最短时间，供前端展示
+    public static final String DEFAULT_SHORTEST_TIME = "1秒";
+
+    // 下次任务执行时间已过
+    public static final String NEXT_TASK_TIME_PAST = "时间已过";
+
+    // 默认的时区（明确指定为北京时间）
+    public static final String DEFAULT_ZONE_ID = "Asia/Shanghai";
+
+    // LocalDateTime类型数据输出的格式
+    public static final String LOCAL_DATE_TIME_OUTPUT_FORMATTER = "yyyy年MM月dd日 HH时mm分ss秒";
 }
