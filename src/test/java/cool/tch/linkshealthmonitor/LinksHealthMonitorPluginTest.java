0f036c20f83ab1293de2e55d0bc8b77c3b851402
denchouka <denchouka@163.com>
2025-08-31 17:58:31 +0800

    Add mock SchemeManager

--- src/test/java/cool/tch/linkshealthmonitor/LinksHealthMonitorPluginTest.java
+++ src/test/java/cool/tch/linkshealthmonitor/LinksHealthMonitorPluginTest.java
@@ -1,10 +1,12 @@
 package cool.tch.linkshealthmonitor;
 
+import cool.tch.linkshealthmonitor.task.LinksHealthMonitorUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.PluginContext;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,6 +17,9 @@ class LinksHealthMonitorPluginTest {
 
     @InjectMocks
     LinksHealthMonitorPlugin plugin;
+
+    @Mock
+    SchemeManager schemeManager;
 
     @Test
     void contextLoads() {
