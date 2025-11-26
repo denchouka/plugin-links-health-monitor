7ba4144aa04211d7c077618d72bfd1ecefd4c142
denchouka <denchouka@163.com>
2025-11-26 20:35:45 +0800

    新增方法

--- ui/src/enum/TaskStatus.ts
+++ ui/src/enum/TaskStatus.ts
@@ -40,3 +40,13 @@ export function isShowResult(status: TaskStatus): boolean {
   // 只有任务成功时才显示
   return status === TaskStatus.COMPLETED;
 }
+
+/**
+ * 是否显示下次任务的执行时间
+ * @param status - 任务状态枚举值
+ * @returns 是否显示下次任务的执行时间
+ */
+export function isShowNextScheduledExecution(status: TaskStatus): boolean {
+  // 已创建,等待执行时不显示
+  return status !== TaskStatus.CREATED
+}
