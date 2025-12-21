116afd95128db3e47bbc4d17baa0f6ea45709009
denchouka <denchouka@163.com>
2025-12-21 18:41:19 +0800

    修改线程池线程数

--- src/main/java/cool/tch/linkshealthmonitor/constant/Constant.java
+++ src/main/java/cool/tch/linkshealthmonitor/constant/Constant.java
@@ -18,8 +18,8 @@ public class Constant {
     public static final String CUSTOM_MODEL_METADATA_NAME_PREFIX = "links-health-monitor-";
 
     // 自定义线程池线程数
-    // 最多只会有2个线程，一个执行旧的配置对应的任务，一个执行新的配置对应的任务，一个备用（极端情况下，单个任务的执行时间超过任务的执行周期）
-    public static final int CUSTOM_THREAD_POOL_SIZE = 3;
+    // 有新的任务时会先停止旧的任务，同一时间最多只会有一个任务（2：冗余配置，防止意外情况）
+    public static final int CUSTOM_THREAD_POOL_SIZE = 2;
 
     // 服务关闭时,等待所有正在执行的任务完成，最多等待的秒数
     public static final int CUSTOM_THREAD_AWAIT_TIME = 60;
